41129c0717161bac5bb2a502ddb4f51c60eb2d92
Sandwicheater577 <michalkostkowski062@gmail.com>
2026-02-07 02:29:30 +0100

    Last steps of BootLoader

--- boot/src/boot.asm
+++ boot/src/boot.asm
@@ -1,7 +1,12 @@
 [BITS 16]
 [ORG 0x7c00]
 
-start 
+
+CODE_OFFSET equ 0x8 
+DATA_OFFSET equ 0x10
+
+
+start:
     cli ;Clear interrupts
     mov ax, 0x0000
     mov ds, ax
@@ -11,20 +16,61 @@ start
     sti ;Enable interrupts
     mov si, msg
 
-
-
-print:
-    lodsb
-    cmp al, 0
-    je done 
-    mov ah, 0x0e
-    int 0x10
-    jmp print
-
-done:
+load_PM:
     cli
-    hlt
-msg: db 'Helloworld', 0
+    lgdt[gdt_descriptor] ;Load GDT
+    mov eax, cr0
+    or al, 1 ;Set PE bit in CR0 to enable protected mode
+    mov cr0, eax
+    jmp CODE_OFFSET:PModeMain 
+
+gdt_start:
+    dd 0x00000000
+    dd 0x00000000
+   
+    ; code segment descriptor
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10011010b ;access byte
+    db 11001111b ;flags  
+    db 0x00
+
+    ; data segment descriptor
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010010b ;access byte
+    db 11001111b ;flags  
+    db 0x00
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[BITS 32]
+PModeMain:
+    mov ax, DATA_OFFSET
+    mov ds, ax
+    mov es, ax
+    mov fs, ax  
+    mov ss, ax 
+    mov gs, ax
+    mov ebp, 0x9C00
+    mov esp, ebp
+
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    jmp $
+
+    ; Print message in protected mode
+    mov esi, msg_pm
+    call print_string
+
 
 times 510 - ($ - $$) db 0
 
